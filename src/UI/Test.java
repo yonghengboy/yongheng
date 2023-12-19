0dc83c9b3b038e84e84b72e3f88281a47107d0b5
yonghengboy <86287594+yonghengboy@users.noreply.github.com>
2023-12-19 15:49:46 +0800

    默认变更列表

---
+++ src/UI/Test.java
@@ -0,0 +1,13 @@
+package UI;
+
+import javax.swing.*;
+
+public class Test {
+    private JComboBox comboBox1;
+    private JScrollBar scrollBar1;
+    private JFormattedTextField test12FormattedTextField;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
