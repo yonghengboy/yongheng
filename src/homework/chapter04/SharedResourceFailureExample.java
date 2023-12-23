40c3b9be7e010374b970547fdcb6929c0bf59e21
yonghengboy <86287594+yonghengboy@users.noreply.github.com>
2023-12-23 10:40:07 +0800

    默认变更列表

---
+++ src/homework/chapter04/SharedResourceFailureExample.java
@@ -0,0 +1,46 @@
+package homework.chapter04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SharedResourceFailureExample {
+    static List<Integer> sharedList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new ListModifier(), "ModifierThread");
+        Thread thread2 = new Thread(new ListReader(), "ReaderThread");
+
+        thread1.start();
+        thread2.start();
+    }
+
+    static class ListModifier implements Runnable {
+        @Override
+        public void run() {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    sharedList.add(i);
+                    Thread.sleep(100);
+                }
+                // 模拟崩溃
+                throw new RuntimeException("Crash!");
+            } catch (Exception e) {
+                System.out.println(Thread.currentThread().getName() + " crashed.");
+            }
+        }
+    }
+
+    static class ListReader implements Runnable {
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    System.out.println(sharedList);
+                    Thread.sleep(100);
+                }
+            } catch (Exception e) {
+                System.out.println(Thread.currentThread().getName() + " encountered an error.");
+            }
+        }
+    }
+}
